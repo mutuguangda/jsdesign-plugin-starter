93301b6e98fc564cde38abe0f0b0ce042f5c2bce
mutuguangda <mutuguangda@foxmail.com>
2023-08-06 04:33:03 +0800

    feat: add jsdesign plugin type

--- src/code.ts
+++ src/code.ts
@@ -1,3 +1,41 @@
-let n: number | string
-n = '123'
-console.log('n', n)
+jsDesign.showUI(__html__, {
+  width: 260,
+  height: 293,
+})
+
+jsDesign.ui.onmessage = (msg) => {
+  if (msg.type === 'create-star')
+    create(msg.count, hexToRgb(msg.hex))
+  if (msg.type === 'cancel')
+    cancel()
+}
+
+function create(count: number, color: RGB) {
+  if (count <= 0)
+    return
+  const frame = jsDesign.createFrame()
+  frame.expanded = true
+  frame.layoutMode = 'HORIZONTAL'
+  frame.fills = []
+  for (let i = 0; i < count; i++) {
+    const star = jsDesign.createStar()
+    star.name = '星形'
+    star.pointCount = 5
+    star.fills = [{ type: 'SOLID', color }]
+    frame.appendChild(star)
+  }
+  jsDesign.currentPage.appendChild(frame)
+  jsDesign.currentPage.selection = [frame]
+  jsDesign.viewport.scrollAndZoomIntoView([frame])
+}
+
+function hexToRgb(hex: string): RGB {
+  const r = parseInt(hex.substring(1, 3), 16) / 255
+  const g = parseInt(hex.substring(3, 5), 16) / 255
+  const b = parseInt(hex.substring(5), 16) / 255
+  return { r, g, b }
+}
+
+function cancel() {
+  jsDesign.closePlugin()
+}
--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -11,8 +11,9 @@ export default function (): Plugin[] {
       configureServer(server) {
         const buildOptions = server.config.inlineConfig
         build(buildOptions).then(() => {
-          writeFileSync('dist/package.json', readFileSync('package.json'), 'utf8')
           exec('npx tsc src/code.ts --outDir dist')
+          const { name, id, version: api } = JSON.parse(readFileSync('package.json', 'utf8'))
+          writeFileSync('dist/manifest.json', JSON.stringify({ name, id, api, main: 'code.js', ui: 'ui.html' }), 'utf8')
         })
       },
     },
